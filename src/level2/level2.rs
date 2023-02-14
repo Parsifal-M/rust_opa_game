4dd8ac1d5130629c816dd1aaa56be4eab25918f8
Peter Macdonald <macdonald.peter90@gmail.com>
2023-02-14 14:13:14 +0100

    Making things nice
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

---
+++ src/level2/level2.rs
@@ -0,0 +1,3 @@
+pub mod my_submodule {
+    // ... module contents ...
+}
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,64 @@
 use std::io;
-use std::process::Command;
 
 mod level1;
 // mod level2;
 
-enum OptionChoice {
-    Play,
-    Quit,
-}
-
 fn main() {
-    println!("Hello! Would you like to play the \x1b[34mOPA\x1b[0m Game?");
-    println!("1. \x1b[32mYes, Please!\x1b[0m");
-    println!("2. \x1b[31mNo thanks, bye!\x1b[0m");
-
-    let selected_option = match get_user_input() {
-        Ok(choice) => match choice.trim() {
-            "1" => OptionChoice::Play,
-            "2" => OptionChoice::Quit,
-            _ => {
-                println!("\x1b[31mInvalid choice\x1b[0m");
-                return;
-            }
-        },
-        Err(e) => {
-            println!("Error: {}", e);
-            return;
-        }
-    };
-
-    match selected_option {
-        OptionChoice::Play => {
-            println!("Please select your level:");
-            println!("1. Level 1");
-            println!("2. Level 2");
-
-            let level_choice = match get_user_input() {
-                Ok(choice) => match choice.trim() {
-                    "1" => level1::run(),
-                    // "2" => level2::run(),
-                    _ => {
-                        println!("\x1b[31mInvalid choice\x1b[0m");
-                        return;
-                    }
-                },
-                Err(e) => {
-                    println!("Error: {}", e);
-                    return;
-                }
-            };
-        },
-        OptionChoice::Quit => println!("\x1b[31mOkay, bye!\x1b[0m"),
-    }
-}
-
-fn get_user_input() -> Result<String, io::Error> {
+    println!("Hello! Would you like to play the OPA Barista Game? (y/n)");
     let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-    Ok(input)
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    if input.trim().to_lowercase() == "y" {
+        let level_modules = vec![
+            level1::run as fn() -> Result<bool, String>,
+            // level2::run as fn() -> Result<bool, String>,
+            /* ... add more levels here ... */
+        ];
+        let mut level = 0;
+        loop {
+            if level == level_modules.len() {
+                println!("Congratulations! You passed all levels.");
+                break;
+            }
+            let module = level_modules[level];
+            match module() {
+                Ok(true) => {
+                    println!("Congratulations! You passed level {}.", level + 1);
+                    println!("What would you like to do?");
+                    println!("1. Continue to the next level");
+                    println!("2. Exit");
+
+                    let mut input = String::new();
+                    io::stdin()
+                        .read_line(&mut input)
+                        .expect("Failed to read line");
+                    match input.trim() {
+                        "1" => {
+                            level += 1;
+                        }
+                        "2" => {
+                            println!("Thanks for playing!");
+                            break;
+                        }
+                        _ => {
+                            println!("Invalid input. Exiting...");
+                            break;
+                        }
+                    }
+                }
+                Ok(false) => {
+                    println!("Quitting at level {}.", level + 1);
+                    println!("Thanks for playing!");
+                    break;
+                }
+                Err(error) => {
+                    println!("Error: {}", error);
+                    break;
+                }
+            }
+        }
+    } else {
+        println!("Thanks for considering the OPA Barista Game. Goodbye!");
+    }
 }
